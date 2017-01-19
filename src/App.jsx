0cf11ef06233f6f984792da4530b08b79ecc9e14
Hyeunjoon Kim <hyeunjoonkim@Hyeunjoons-MacBook-Pro-2.local>
2017-01-18 21:45:45 -0800

    added mobile detection by user agent

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import TopNav from './TopNav';
 import './App.scss';
+import { isMobile } from './utils/utils';
 
 class App extends Component {
+  componentDidMount() {
+    if (isMobile.any()) {
+        alert('UH OH. This site is NOT mobile optimized, but I may have left some mobile only easter eggs ^_~');
+    }
+    console.log('---------------------------------', 'https://ca.linkedin.com/in/hyeunjoon-kim-a3117769', '---------------------------------');
+  }
+
   render() {
     return (
       <div className="app">
