ee60edec9c5a7f005db8172f00e37304840313bd
Hyeunjoon Kim <hyeunjoonkim@Hyeunjoons-MacBook-Pro-2.local>
2017-01-18 19:46:41 -0800

    add react router and not found page

---
+++ src/NotFound.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The page you are looking for could not be found.</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import App from './App';
+import NotFound from './NotFound';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App} />
+        <Route path="*" component={NotFound}/>
+    </Router>),
   document.getElementById('root')
 );
