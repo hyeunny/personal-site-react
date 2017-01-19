73722f8da4902bbaf82fd162b9dc0fe21b4d16a4
Hyeunjoon Kim <hyeunjoonkim@Hyeunjoons-MacBook-Pro-2.local>
2017-01-19 00:03:49 -0800

    added random acronym expander

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import mugShot from './images/hj_mugshot.jpg';
 import './App.scss';
+import AcronymExpander from './components/AcronymExpander';
+
+const returnRandomColor = (colors = ['white', 'orange', 'yellow', 'green']) => {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        style: {color: returnRandomColor()}
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => { 
+        this.setState({style: {color: returnRandomColor()}}) }
+    , 200);
+  }
+
   render() {
     return (
       <div className="home">
-        <h1 className="greeting top">Hello, I am Hyeunjoon.</h1>
-          <img className="mugshot" alt="myMug" src={mugShot} />
-        <h1 className="greeting bottom">..and I love building things on the web.</h1>
+        <div className="greeting-container">
+            <h1 className="greeting top">Hello, I am Hyeunjoon.</h1>
+                <img className="mugshot" alt="myMug" src={mugShot} />
+            <h1 className="greeting bottom">..and I love building things on the web.</h1>
+        </div>
+        <AcronymExpander style={this.state.style} enteredWord="Hyeunjoon" />
       </div>
     );
   }
---
+++ src/components/AcronymExpander.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+const DICTIONARY = {
+  A: ['adventurous', 'alert', 'advanced', 'alien'],
+  B: ['badass', 'brave', 'battle-tested', 'bone'],
+  C: ['crispy', 'corny', 'calculating', 'crepe'],
+  D: ['daft', 'deadly', 'dangerous', 'door'],
+  E: ['energetic', 'entrepreneurial', 'electric', 'eel'],
+  F: ['fabulous', 'flatulent', 'fantastic', 'forest'],
+  G: ['great', 'good', 'grand', 'gallon'],
+  H: ['happy', 'hearty', 'hot', 'hippo', 'hot_module_reloading'],
+  I: ['intelligent', 'interesting', 'impeccable', 'imp'],
+  J: ['jaded', 'jovial', 'joyous', 'jargon', 'jest_test_runner'],
+  K: ['killer', 'korean', 'kite'],
+  L: ['likable', 'lovable', 'lizard'],
+  M: ['maniacal', 'martian', 'mastermind', 'marble'],
+  N: ['natural', 'ninja', 'null', 'natural_language_processing'],
+  O: ['orange', 'overlord', 'oligarchy', 'oreo', 'orifice'],
+  P: ['pineapple', 'platypus', 'play', 'parchment'],
+  Q: ['qualified', 'questing', 'queen', 'quench'],
+  R: ['royal', 'rock', 'restraint', 'right'],
+  S: ['super', 'starlike', 'sadness', 'solitude'],
+  T: ['terrible', 'tuba', 'territory'],
+  U: ['ultra', 'understanding', 'umbrella', 'universal_module_definition'],
+  V: ['very', 'varsity', 'varnish'],
+  W: ['winning', 'warrior', 'wednesday', 'wordpress'],
+  X: ['xylophone', 'xanax', 'xml'],
+  Y: ['yellow', 'yucky', 'yarn', 'yaml', 'yet_another_markup_language'],
+  Z: ['zoo', 'zimbabwe', 'zoom'],
+}
+
+class AcronymExpander extends Component {
+  render() {
+    const {
+      enteredWord,
+      style
+    } = this.props;
+
+    const indivLetters = enteredWord.toUpperCase().split('');
+
+    return (
+      <ul 
+        className="acronym-expander"
+        style={style}
+      >
+        {indivLetters.map((letter, idx) => {
+          return <li key={idx}>{DICTIONARY[letter][Math.floor(Math.random()*DICTIONARY[letter].length)].toUpperCase()}</li>
+        })}
+      </ul>
+    );
+  }
+}
+
+AcronymExpander.propTypes = {
+  enteredWord: React.PropTypes.string
+};
+
+export default AcronymExpander;
