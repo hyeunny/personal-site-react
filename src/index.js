fa688bec3ae0d0a6b5466b1cf007752422aabe6e
Hyeunjoon Kim <hyeunjoonkim@Hyeunjoons-MacBook-Pro-2.local>
2017-01-18 20:25:52 -0800

    added webfont and baseline css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
+import './index.css';
 import App from './App';
 import NotFound from './NotFound';
-import './index.css';
 
 ReactDOM.render((
     <Router history={browserHistory}>
