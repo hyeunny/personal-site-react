4a580b28890459ffebbacb3e7b41a37c2ff0970d
Hyeunjoon Kim <hyeunjoon.kim@mail.mcgill.ca>
2017-01-22 20:03:39 -0800

    added client server split

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/api/test', (req, res) => {
+  console.log('TEST!');
+  res.send('hello world!!!! yoyoyoyoyowtfwtf');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
